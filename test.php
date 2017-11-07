96e97d11b5e6122237dd47f93b21f2cea1f86a26
gaoshikai <kaimissgao@qqdyw.net>
2017-11-07 21:01:10 +0800

    初始化

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-use Loliko\MtaApi;
-$mmd = new MtaApi(500028297 , '9c8db5792f750ae4aace861b85507415');
-print_r($mmd->getUserPortrait('2017-11-06' , '2017-11-07' , 'grade'));
