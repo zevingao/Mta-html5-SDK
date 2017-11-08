d6eb3c98e76c72fd16270c98dbbeeeb2e1164194
gaoshikai <kaimissgao@qqdyw.net>
2017-11-08 17:57:29 +0800

    初始化

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # Mta-html5-SDK
+
+此项目是腾讯mta平台html5页面统计数据接口sdk，需要php>=5.4。
+<br/>
+详细使用方式:<br/>
+```
+第一步：在composer.json里加入扩展包
+"require": {
+    	"loliko/mta-html5-sdk" : "dev-master"
+    }
+    
+第二步：composer update
+
+第三步：代码示例
+require 'vendor/autoload.php';
+use Loliko\MtaApi;
+
+$mmd = new MtaApi(你的mta应用appid , '你的mta应用secret');
+print_r($mmd->historyData('2017-11-06' , '2017-11-07' , 'uv'));
+
+```
+
+详细接口请求方式和需要的参数信息以及错误码请查阅[接口文档](http://docs.developer.qq.com/mta/fast_access/h5/api.html)
+##接口说明
+####接口定义
+<br/>
+ * Func<br/>
+ * public historyData       应用历史趋势数据<br/>
+ * public hourData          应用小时数据<br/>
+ * public heartData         应用心跳数据<br/>
+ * public 实时访客           realTimeData<br/>
+ * public 新老访客比          compareData<br/>
+ * public 用户画像           getUserPortrait<br/>
+ * public 地区数据           areaData<br/>
+ * public 省市数据           provinceData<br/>
+ * public 运营商数据         operatorData<br/>
+ * public 终端属性列表        paraData<br/>
+ * public 终端信息           paraContentData<br/>
+ * public 终端属性列表        pageData<br/>
+ * public 性能监控           pageSpeedData<br/>
+ * public 访问深度           depthData<br/>
+ * public 外部链接           sourceData<br/>
+ * public 入口页面           landData<br/>
+ * public 离开页面           exitData<br/>
+ * public 自定义时间         customData<br/>
+ * public 渠道效果分析        adTagData<br/>
+ 
+####接口参数
+请根据开发文档以及函数声明传值
+
+#####注意
+不能根据返回的code或者info判断接口是否成功，有的方法没有返回这两个数据
+
+
+
+## TODO
+1.错误处理
+<br/>
+
+
+## 更新进度
+
+
+-----------------------------2017-11-08----------------------------
+<br/>
+1.初始化项目
+<br/>
--- src/MtaApi.php
+++ src/MtaApi.php
@@ -1,10 +1,30 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: loliko
- * Date: 17-11-7
- * Time: 下午8:03
  * mta平台html5应用统计api sdk，详情接口信息及错误说明请访问 http://docs.developer.qq.com/mta/fast_access/h5/api.html
+ * Date:    17-11-7
+ * Author:  loliko
+ * Ver:     1.0
+ *
+ * Func
+ * public historyData       应用历史趋势数据
+ * public hourData          应用小时数据
+ * public heartData         应用心跳数据
+ * public 实时访客           realTimeData
+ * public 新老访客比          compareData
+ * public 用户画像           getUserPortrait
+ * public 地区数据           areaData
+ * public 省市数据           provinceData
+ * public 运营商数据         operatorData
+ * public 终端属性列表        paraData
+ * public 终端信息           paraContentData
+ * public 终端属性列表        pageData
+ * public 性能监控           pageSpeedData
+ * public 访问深度           depthData
+ * public 外部链接           sourceData
+ * public 入口页面           landData
+ * public 离开页面           exitData
+ * public 自定义时间         customData
+ * public 渠道效果分析        adTagData
  */
 
 namespace Loliko;
@@ -39,6 +59,46 @@ class MtaApi
     //用户画像
     const USER_PORTRAIT = 'http://mta.qq.com/h5/api/ctr_user_portrait';
 
+    //客户端分析,地区数据,按天查询地区的pv\uv\vv\iv量
+    const CTR_AREA_API = 'http://mta.qq.com/h5/api/ctr_area/get_by_area';
+
+    //省市数据,按天查询省市下有流量的城市的pv\uv\vv\iv量
+    const CTR_PROVINCE_API = 'http://mta.qq.com/h5/api/ctr_area/get_by_province';
+
+    //运营商,按天查询运营商的pv\uv\vv\iv量
+    const CTR_OPERATOR_API = 'http://mta.qq.com/h5/api/ctr_operator';
+
+    //终端属性列表,按天查询对应属性的终端信息数据
+    const CTR_PARA_API = 'http://mta.qq.com/h5/api/ctr_client/get_by_para';
+
+    //按天查询终端信息数据
+    const CTR_CLIENT_API = 'http://mta.qq.com/h5/api/ctr_client/get_by_content';
+
+    //页面排行,按天查询url的pv\uv\vv\iv数据
+    const CTR_PAGE_API = 'http://mta.qq.com/h5/api/ctr_page';
+
+    //性能监控,按天查询对应省市的访问延时与解析时长
+    const CTR_SPEED_API = 'http://mta.qq.com/h5/api/ctr_page_speed';
+
+    //访问深度,按天查询用户访问深度
+    const CTR_DEPTH_API = 'http://mta.qq.com/h5/api/ctr_depth';
+
+    //来源分析,外部链接,按天查询外部同站链接带来的流量情情况
+    const CTR_SOURCE_API = 'http://mta.qq.com/h5/api/ctr_source_out';
+
+    //入口页面,按天查询用户最后访问的进入次数与跳出率
+    const CTR_LAND_API = 'http://mta.qq.com/h5/api/ctr_page_land';
+
+    //离开页面,按天查询最后访问页面的离次数
+    const CTR_PAGEEXIT_API = 'http://mta.qq.com/h5/api/ctr_page_exit';
+
+    //自定义事件,按天查询自定义事件的pv\uv\vv\iv
+    const CTR_CUSTOM_API = 'http://mta.qq.com/h5/api/ctr_custom';
+
+    //渠道效果统计,渠道效果分析,按天查询渠道的分析数据
+    const CTR_ADTAG_API = 'http://mta.qq.com/h5/api/ctr_adtag';
+
+
     /**
      * MtaApi constructor.
      * @param $app_id
@@ -51,6 +111,98 @@ class MtaApi
         $this->_httpClient = new GuzzleHttp\Client();
     }
 
+    /**
+     * 应用历史趋势数据
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
+    public function historyData($startDate , $endDate , $idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'idx' => $idx,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_CORE_API , $params);
+    }
+
+
+    /**
+     * 应用小时数据
+     * @param $idx
+     * @return array|mixed
+     */
+    public function hourData($idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'idx' => $idx,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::GET_BY_HOUR_API , $params);
+    }
+
+    /**
+     * 应用心跳数据
+     * @return array|mixed
+     */
+    public function heartData()
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::HEART_BEAT_API , $params);
+    }
+
+    /**
+     * 实时访客
+     * @param $page
+     * @return array|mixed
+     */
+    public function realTimeData($page)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'page' => $page,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_REALTIME , $params);
+    }
+
+    /**
+     * 新老访客比
+     * @param $startDate
+     * @param $endDate
+     * @return array|mixed
+     */
+    public function compareData($startDate , $endDate)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_COMPARE , $params);
+    }
+
+    /**
+     * 用户画像
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
     public function getUserPortrait($startDate , $endDate , $idx)
     {
         $params = [
@@ -64,6 +216,294 @@ class MtaApi
         return $this->_request(self::USER_PORTRAIT , $params);
     }
 
+    /**
+     * 地区数据
+     * @param $typeIds
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
+    public function areaData($typeIds , $startDate , $endDate , $idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'idx' => $idx,
+            'type_ids' => $typeIds,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_AREA_API , $params);
+    }
+
+    /**
+     * 省市数据
+     * @param $typeIds
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
+    public function provinceData($typeIds , $startDate , $endDate , $idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'idx' => $idx,
+            'type_ids' => $typeIds,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_PROVINCE_API , $params);
+    }
+
+    /**
+     * 运营商数据
+     * @param $typeIds
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
+    public function operatorData($typeIds , $startDate , $endDate , $idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'idx' => $idx,
+            'type_ids' => $typeIds,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_OPERATOR_API , $params);
+    }
+
+    /**
+     * 终端属性列表
+     * @param $typeId
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
+    public function paraData($typeId , $startDate , $endDate , $idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'idx' => $idx,
+            'type_id' => $typeId,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_PARA_API , $params);
+    }
+
+    /**
+     * 终端信息
+     * @param $typeId
+     * @param $typeContents
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
+    public function paraContentData($typeId , $typeContents , $startDate , $endDate , $idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'idx' => $idx,
+            'type_id' => $typeId,
+            'type_contents' => $typeContents,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_PARA_API , $params);
+    }
+
+    /**
+     * 终端属性列表
+     * @param $urls
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
+    public function pageData($urls , $startDate , $endDate , $idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'idx' => $idx,
+            'urls' => $urls,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_PAGE_API , $params);
+    }
+
+    /**
+     * 性能监控
+     * @param $type
+     * @param $typeContents
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
+    public function pageSpeedData($type , $typeContents , $startDate , $endDate , $idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'idx' => $idx,
+            'type' => $type,
+            'type_contents' => $typeContents,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_SPEED_API , $params);
+    }
+
+    /**
+     * 访问深度
+     * @param $startDate
+     * @param $endDate
+     * @return array|mixed
+     */
+    public function depthData($startDate , $endDate)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_DEPTH_API , $params);
+    }
+
+    /**
+     * 外部链接
+     * @param $urls
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
+    public function sourceData($urls , $startDate , $endDate , $idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'idx' => $idx,
+            'urls' => $urls,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_SOURCE_API , $params);
+    }
+
+    /**
+     * 入口页面
+     * @param $urls
+     * @param $startDate
+     * @param $endDate
+     * @return array|mixed
+     */
+    public function landData($urls , $startDate , $endDate)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'urls' => $urls,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_LAND_API , $params);
+    }
+
+    /**
+     * 离开页面
+     * @param $urls
+     * @param $startDate
+     * @param $endDate
+     * @return array|mixed
+     */
+    public function exitData($urls , $startDate , $endDate)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'urls' => $urls,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_PAGEEXIT_API , $params);
+    }
+
+    /**
+     * 自定义时间
+     * @param $custom
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
+    public function customData($custom , $startDate , $endDate , $idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'idx' => $idx,
+            'custom' => $custom,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_CUSTOM_API , $params);
+    }
+
+    /**
+     * 渠道效果分析
+     * @param $adTags
+     * @param $startDate
+     * @param $endDate
+     * @param $idx
+     * @return array|mixed
+     */
+    public function adTagData($adTags , $startDate , $endDate , $idx)
+    {
+        $params = [
+            'app_id' => $this->_app_id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'idx' => $idx,
+            'adtags' => $adTags,
+        ];
+
+        $params['sign'] = $this->_makeSign($params);
+        return $this->_request(self::CTR_ADTAG_API , $params);
+    }
+
+    /**
+     * 请求接口方法
+     * @param $url 接口地址
+     * @param $params 接口参数
+     * @return array|mixed
+     */
     private function _request($url , $params)
     {
         $result = $this->_httpClient->request('GET' , $url , ['query' => $params])->getBody()->getContents();
@@ -75,7 +515,7 @@ class MtaApi
 
     /**
      * 封装签名sign
-     * @param $paramArr 需要发送的数据包
+     * @param $params 需要发送的数据包
      */
     private function _makeSign($params){
         $secret = $this->_secret_key;
